10d21feccb45f45f85be664c0d103ee0177fcc97
charles <charlesmaxwellyoung@gmail.com>
2022-02-27 23:08:28 +0800

    feat: 构建播客初始资源

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: "半左偏右",
+  description: "半左偏右, 用于记录生活、技术、感想、笔记的播客",
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@docs": "docs",
+      },
+    },
+  },
+  plugins: [
+    "@vuepress/active-header-links",
+    {
+      sidebarLinkSelector: ".sidebar-link",
+      headerAnchorSelector: ".header-anchor",
+    },
+    "@vuepress/back-to-top",
+    "@vuepress/medium-zoom",
+    "@vuepress/nprogress",
+  ],
+};
